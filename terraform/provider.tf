9f4d483d67c3a5d7c35eeafb029175adc8b47bf4
Jakub <kubasurdej@gmail.com>
2023-02-05 11:01:43 +0100

    Move to terraform cloud

---
+++ terraform/provider.tf
@@ -0,0 +1,9 @@
+terraform {
+  cloud {
+    organization = "kubasurdej"
+
+    workspaces {
+      name = "surdej-me"
+    }
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "do_token" {
   description = "DigitalOcean API Token"
   type        = string
 }
+
+variable "terraform_cloud_org" {
+  description = "Terraform Cloud organization name"
+  type        = string
+}
+
+variable "terraform_cloud_workspace" {
+  description = "Terraform Cloud workspace name"
+  type        = string
+}
