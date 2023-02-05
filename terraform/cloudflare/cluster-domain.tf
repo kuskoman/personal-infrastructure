b68e36608527ee167812b84e951f07dba9346263
Jakub <kubasurdej@gmail.com>
2023-02-05 13:19:41 +0100

    Add records for cluster

--- terraform/cloudflare/cluster-domain.tf
+++ terraform/cloudflare/cluster-domain.tf
@@ -1,3 +1,23 @@
 resource "cloudflare_zone" "main_domain_zone" {
   zone = var.main_domain
 }
+
+resource "cloudflare_zone" "argo_domain_zone" {
+  zone = "argo.${var.main_domain}"
+}
+
+resource "cloudflare_record" "cluster_main_domain" {
+  zone_id = cloudflare_zone.main_domain_zone.id
+  name    = var.main_domain
+  type    = "A"
+  value   = var.ingress_lb_ip
+  ttl     = 1
+}
+
+resource "cloudflare_record" "cluster_argo_domain" {
+  zone_id = cloudflare_zone.argo_domain_zone.id
+  name    = "argo.${var.main_domain}"
+  type    = "A"
+  value   = var.ingress_lb_ip
+  ttl     = 1
+}
