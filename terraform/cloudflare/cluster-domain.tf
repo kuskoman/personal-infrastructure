5b153a3e75f2e3f3c8de983ac0d8ebfa5c4d470d
Jakub <kubasurdej@gmail.com>
2023-02-25 13:11:49 +0100

    Add wildcard to cluster domain

--- terraform/cloudflare/cluster-domain.tf
+++ terraform/cloudflare/cluster-domain.tf
@@ -10,3 +10,10 @@ resource "cloudflare_record" "cluster_main_domain" {
   ttl     = 1
 }
 
+resource "cloudflare_record" "cluster_wildcard_domain" {
+  zone_id = data.cloudflare_zone.main_domain_zone.id
+  name    = "*.${var.main_domain}"
+  type    = "A"
+  value   = var.ingress_lb_ip
+  ttl     = 1
+}
